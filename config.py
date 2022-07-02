e4b390ee50bc8002fe1547e626832cef335e0391
Irina_Strong_Lady <iogontar@gmail.com>
2022-07-02 14:00:11 +0300

    Chapter 1: Initial commit

---
+++ config.py
@@ -0,0 +1,46 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'e953491d3845fa0167c0b5d6b8b970a1b0ae32b7'
+    SQLALCHEMY_COMMIT_ON_TEARDOWN = True
+    FKASKY_MAIL_SUBJECT_PREFIX = '[Flasky]'
+    FLASKY_MAIL_SENDER = os.environ.get('MAIL_USERNAME')
+    FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
+    BOOTSTRAP_SERVE_LOCAL = True
+
+    @staticmethod
+    def init_app():
+        pass
+
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+    MAIL_SERVER = 'post.mvd.ru'
+    MAIL_PORT = 'smtp'
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'data-dev.sqlite')
+
+
+class TestingConfig(Config):
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('TEST_DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'data-test.sqlite')
+
+
+class ProductionConfig(Config):
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+
+
+config = {
+    'development': DevelopmentConfig,
+    'testing': TestingConfig,
+    'production': ProductionConfig,
+
+    'default': DevelopmentConfig
+}
