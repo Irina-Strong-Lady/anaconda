a89647645d16f885485f625f07904ee7f934ba62
Irina_Strong_Lady <iogontar@gmail.com>
2022-07-15 18:27:54 +0300

    Chapter 1: Change current email-address by confirmation with new email-address working. Working version in complete.

--- app/models.py
+++ app/models.py
@@ -71,6 +71,17 @@ class User(UserMixin, db.Model):
     def is_administrator(self):
         return self.can(Permission.ADMINISTER)
 
+    @property
+    def password(self):
+        raise AttributeError('Password is not readable attribute')
+
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password)    
+
     def generate_confirmation_token(self, expiration=3600):
         s = Serializer(current_app.config['SECRET_KEY'], expiration)
         return s.dumps({'confirm': self.id})
@@ -105,17 +116,27 @@ class User(UserMixin, db.Model):
         db.session.add(user)
         return True 
 
-    @property
-    def password(self):
-        raise AttributeError('Password is not readable attribute')
+    def generate_email_change_token(self, new_email, expiration=3600):
+        s = Serializer(current_app.config['SECRET_KEY'], expiration)        
+        return s.dumps(
+            {'change_email': self.id, 'new_email': new_email}).decode('utf-8')
 
-    @password.setter
-    def password(self, password):
-        self.password_hash = generate_password_hash(password)
-
-    def verify_password(self, password):
-        return check_password_hash(self.password_hash, password)
-
+     
+    def change_email(self, token):
+        s = Serializer(current_app.config['SECRET_KEY'])
+        try:
+            data = s.loads(token.encode('utf-8'))
+        except:
+            return False
+        if data.get('change_email') != self.id:
+            return False
+        new_email = data.get('new_email')
+        if new_email is None:
+            return False
+        self.email = new_email
+        db.session.add(self)
+        return True       
+    
     def __repr__(self):
         return '<User %r>' % self.username
 
