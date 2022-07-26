781703b17a2c962db1d97a63ce6562b2a83938bb
Irina_Strong_Lady <iogontar@gmail.com>
2022-07-26 19:24:41 +0300

    Chapter 4. Avatars needed web to check it working.

--- app/models.py
+++ app/models.py
@@ -1,10 +1,11 @@
 from . import db
 from werkzeug.security import generate_password_hash, check_password_hash
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
-from flask import current_app
+from flask import current_app, request
 from flask_login import UserMixin, AnonymousUserMixin
 from . import login_manager
 from datetime import datetime
+import hashlib
 
 
 class Permission:
@@ -80,6 +81,7 @@ class User(UserMixin, db.Model):
     about_me = db.Column(db.Text())
     member_since = db.Column(db.DateTime(), default=datetime.utcnow)
     last_seen = db.Column(db.DateTime(), default=datetime.utcnow)
+    avatar_hash = db.Column(db.String(32))
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
 
     def __init__(self, **kwargs):
@@ -89,6 +91,8 @@ class User(UserMixin, db.Model):
                 self.role = Role.query.filter_by(name='Administrator').first()
             if self.role is None:
                 self.role = Role.query.filter_by(default=True).first()
+        if self.email is not None and self.avatar_hash is None:
+            self.avatar_hash = self.gravatar_hash()
 
     
     def can(self, perm):
@@ -158,13 +162,28 @@ class User(UserMixin, db.Model):
         new_email = data.get('new_email')
         if new_email is None:
             return False
+        if self.query.filter_by(email=new_email).first() is not None:
+            return False
         self.email = new_email
+        self.avatar_hash = self.gravatar_hash()
         db.session.add(self)
-        return True 
-
-    def ping(self)      :
+        return True
+        
+    def ping(self):
         self.last_seen = datetime.utcnow()
         db.session.add(self)
+
+    def gravatar_hash(self):
+        return hashlib.md5(self.email.lower().encode('utf-8')).hexdigest()
+
+    def gravatar(self, size=100, default='identicon', rating='g'):
+        if request.is_secure:            
+            url = 'https://secure.gravatar.com/avatar'
+        else:
+            url = 'http://www.gravatar.com/avatar'  
+            hash = self.avatar_hash or self.gravatar_hash()
+            return '{url}/{hash}?s={size}&d={default}&r={rating}'.format(
+                url=url, hash=hash, size=size, default=default, rating=rating)
     
     def __repr__(self):
         return '<User %r>' % self.username
