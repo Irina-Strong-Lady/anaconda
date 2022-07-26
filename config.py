3cca2dff249a39712920d2b1ecb21bf3e4f70178
Irina_Strong_Lady <iogontar@gmail.com>
2022-07-27 00:02:31 +0300

    Chapter 4. Avatars, position, moment working. Working version in complete.

--- config.py
+++ config.py
@@ -19,7 +19,7 @@ class Config:
 class DevelopmentConfig(Config):
     DEBUG = True
     MAIL_SERVER = os.environ.get('MAIL_SERVER')
-    MAIL_PORT = os.environ.get('MAIL_PORT')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT'))
     MAIL_USE_TLS = True
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
